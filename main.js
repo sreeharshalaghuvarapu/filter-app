2784ddb9c8a85148178e54fdc21336d3c4c0411d
sreeharshalaghuvarapu <72685506+sreeharshalaghuvarapu@users.noreply.github.com>
2021-08-29 18:55:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,52 @@
+noseX=0;
+noseY=0;
+
+
 function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/RZz6MBfR/Icon-clown-nose.png');
 }
 
 function setup() {
-    canvas = createCanvas(500,400);
+    canvas = createCanvas(300,300);
     canvas.center();
     video = createCapture(VIDEO)
-    video.size(500, 400)
+    video.size(300, 300)
     video.hide();
 
     poseNet = ml5.poseNet(video, modelloded);
-    poseNet.on('pose',gotposes);
+    poseNet.on('pose',gotPoses);
 }
 
-function gotposes(results)
+{
+    function modelloded() {
+        console.log('Posenet Is Instalized');
+    }
+}
+
+
+function gotPoses(results)
 {
     if(results.length > 0)
     {
     console.log(results)
+    noseX = results[0].pose.noseX;
+    noseY = results[0].pose.noseY;
     console.log("nose x = " + results[0].pose.nose.x);
     console.log("nose y = " + results[0].pose.nose.y);
     }
 }
 
-function modelloded() {
-    console.log('poseNet Is Initialized');
+function draw(){
+ image(video, 0, 0, 300, 300);
+ image(clown_nose, noseX,noseY,30,30)
 }
 
-function draw() {
-image(video, 0, 0, 500, 400);
+function take_snapshot(){
+    save(myfilterphoto.png);
+}
+
+function modelloded() {
+    console.log('poseNet Is Initialized');
 }
 
 function take_snapshot(){
